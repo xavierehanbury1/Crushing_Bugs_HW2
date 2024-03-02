4ed98c7f3ce92ce73f413302e2f34fea4545b4d1
xavierehanbury1 <113371075+xavierehanbury1@users.noreply.github.com>
2024-03-02 17:24:27 -0500

    Update main.js

--- CrushingBugs/js/main.js
+++ CrushingBugs/js/main.js
@@ -18,15 +18,15 @@ let theButtons = document.querySelectorAll("#buttonHolder img"),
 // step 3
 // functionality always goes in the middle -> how do we want
 // the app to behave?
-function changeBGImage() {
+//function changeBGImage() {
 	// the `` is a JavaScript template string. It tells the JS enging to evaluate the expression
 	// inside the braces - run that little bit of code. In this case it's just pulling the ID of the
 	// button we clicked on and putting it at the end of the image name (0, 1, 2, 3)
 	// and updating the background-image style of the puzzle board element.
-
+	
 	// bug fix #2 should go here. it's at most 3 lines of JS code.
-	puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
-}
+	//puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
+//}
 
 function handleStartDrag() { 
 	console.log('started dragging this piece:', this);
